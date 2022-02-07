49458cd2adee158aacb1f53b147cd020ae33ec7c
bartekGardas <bartek.gardas@gmail.com>
2022-02-07 23:49:29 +0100

    add KerrlOscillators

--- src/SB.jl
+++ src/SB.jl
@@ -1,3 +1,13 @@
 # SB.jl: different algorithms for simulated bifurcation.
+export
+    KerrlOscillators
 
 # https://www.science.org/doi/pdf/10.1126/sciadv.aav2372
+
+struct KerrlOscillators{T <: Real}
+    ig::IsingGraph
+    Kerr_coefficient::T
+    detuning::Vector{T}
+    scale::T
+    pump::Function
+end
