48a37bda37e5428993588c2aa510b2120ebe5cd0
bartekGardas <bartek.gardas@gmail.com>
2022-02-08 11:19:01 +0100

    add kerr oscilators and chaos

--- src/SB.jl
+++ src/SB.jl
@@ -2,38 +2,67 @@
 export
     KerrOscillators,
     KPODynamics,
-    evolve_kerr_oscillators
+    evolve_kerr_oscillators,
+    naive_evolve_kerr_oscillators
 
 # https://www.science.org/doi/pdf/10.1126/sciadv.aav2372
 struct KerrOscillators{T <: Real}
     ig::IsingGraph
     kerr_coeff::T
     detuning::T
+    pump::Function
     scale::T
 end
 struct KPODynamics{T <: Real}
     init_state::Vector{T}
     num_steps::Int
-    pump::Function
     dt::T
 end
 
-function evolve_kerr_oscillators(
-    kpo::KerrOscillators{T},
-    dyn::KPODynamics
-) where T <: Real
+# https://www.science.org/doi/epdf/10.1126/sciadv.abe7953
+# This procedure uses the symplectic Euler method,
+# which make it potentially fast for CPUs/ GPU / FPGA.
+function evolve_kerr_oscillators(kpo::KerrOscillators{T}, dyn::KPODynamics) where T <: Real
     N = length(dyn.init_state)
     @assert N % 2 == 0
 
-    J = couplings(kpo.ig)
+    J = -couplings(kpo.ig)
     x = dyn.init_state[1:N÷2]
     y = dyn.init_state[N÷2+1:end]
 
     for i ∈ 1:dyn.num_steps
         x .+= kpo.detuning .* y .* dyn.dt
         Φ = kpo.scale .* J * x
-        p = dyn.pump(i * dyn.dt)
+        p = kpo.pump((i+1) * dyn.dt)
         y .-= (kpo.kerr_coeff .* x .^ 3 .+ (kpo.detuning - p) .* x .- Φ) .* dyn.dt
     end
     Int.(sign.(x))
+end
+
+# This procedure does the same as the evolve_kerr_oscillators.
+# It uses DifferentialEquations engine to solve ODEs. This is
+# slow but potentially accurate to an arbitrary precision.
+function kerr_system(u, kpo, t)
+    J = -couplings(kpo.ig)
+    N = length(u) ÷ 2
+    x = @view u[1:N]
+    y = @view u[N+1:end]
+    Φ = kpo.scale .* J * x
+    vcat(
+        kpo.detuning .* y,
+        -(kpo.kerr_coeff .* x .^ 2 .+ (kpo.detuning - kpo.pump(t))) .* x .+ Φ
+    )
+end
+
+function naive_evolve_kerr_oscillators(
+    kpo::KerrOscillators{T},
+    dyn::KPODynamics,
+    args=()
+) where T <: Real
+    tspan = (0., dyn.num_steps * dyn.dt)
+    ode = ODEProblem(kerr_system, dyn.init_state, tspan, kpo)
+    sol = solve(ode, save_everystep=false, args...)
+    x = sol.u[end]
+    N = length(x) ÷ 2
+    Int.(sign.(x[1:N]))
 end
--- test/SB.jl
+++ test/SB.jl
@@ -1,33 +1,41 @@
+using DifferentialEquations
+using SpinGlassNetworks
+
 @testset "simulated bifurcation simulator for chimera instances." begin
     L = 128
 
-    ig = ising_graph("$(@__DIR__)/instances/chimera_droplets/$(L)power/002.txt") # no biases
+    ig = ising_graph("$(@__DIR__)/instances/chimera_droplets/$(L)power/002.txt") # no biases, E = -210.13
+    #ig = ising_graph("$(@__DIR__)/instances/basic/16_001.txt") # no biases, E = -4.625
+
+    J = couplings(ig)
+    Js = sqrt(sum(J .^ 2) / L / (L-1))
 
     kerr_coeff = 1.
     detuning = 1.
-    scale =  0.7 / sqrt(L)
+    scale = 0.5 / sqrt(L) / Js
 
-    init_state = zeros(2 * L)
+    init_state = rand(2 * L)
     num_steps = 100
-    pump = t -> 2 * tanh(2 * t / L)
-    dt = 0.1
+    dt = 0.25
+    pump = t -> t / num_steps / dt
 
-    kpo = KerrOscillators{Float64}(ig, kerr_coeff, detuning, scale)
-    dyn = KPODynamics{Float64}(init_state, num_steps, pump, dt)
+    kpo = KerrOscillators{Float64}(ig, kerr_coeff, detuning, pump, scale)
+    dyn = KPODynamics{Float64}(init_state, num_steps, dt)
 
-    N = 1
+    N = 100
     states = Vector{Vector{Int}}(undef, N)
-    #Threads.@threads
-    for i ∈ 1:N
+    states_naive = copy(states)
+    Threads.@threads for i ∈ 1:N
         states[i] = evolve_kerr_oscillators(kpo, dyn)
+        states_naive[i] = naive_evolve_kerr_oscillators(kpo, dyn)
     end
 
     en = minimum(energy(states, ig))
-    en_exact = -210.13333399999996
+    en_naive = minimum(energy(states_naive, ig))
 
     #@testset "Energy found is at least negative and within the bounds" begin
     #    @test  en_exact <= en < 0.
     #end
-    println(states[1])
-    println("kpo: ", en)
+    println("kpo: ", en,)
+    println("naive kpo: ", en_naive,)
 end
