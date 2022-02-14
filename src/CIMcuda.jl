bbafe3cd44a493041c74e043d325e14f44e46094
bartekGardas <bartek.gardas@gmail.com>
2022-02-14 13:42:04 +0100

    add opo_kernel

--- src/CIMcuda.jl
+++ src/CIMcuda.jl
@@ -4,7 +4,7 @@ export
 """
 This is CUDA kernel to evolve OPO.
 """
-function opo_kernel(x, y, m, states, J, h, pump, noise, fparams, iparams)
+function opo_kernel(x, states, J, h, pump, noise, fparams, iparams)
     idx = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     x_stride = gridDim().x * blockDim().x
 
@@ -16,13 +16,14 @@ function opo_kernel(x, y, m, states, J, h, pump, noise, fparams, iparams)
     num_steps, num_rep = iparams
 
     for i ∈ idx:x_stride:L, j ∈ idy:y_stride:num_rep
+        Δm = 0.0
         for k ∈ 1:num_steps
             Φ = 0.0
             for l ∈ 1:L @inbounds Φ += J[i, l] * x[l, j] + h[i] end
-            @inbounds Δx[i, j] = pump[k] * x[i, j] + ξ * Φ + noise[i]
-            @inbounds m[i, j] = (1 - α) * Δx[i, j] + α * Δm[i, j]
-            @inbounds x[i, j] += m[i, j] * (abs(x[i, j] + m[i, j]) < 1.0)
-            @inbounds Δm[i, j] = m[i, j]
+            @inbounds Δx = pump[k] * x[i, j] + ξ * Φ + noise[i, j]
+            @inbounds m = (1 - α) * Δx + α * Δm
+            @inbounds x[i, j] += m * (abs(x[i, j] + m) < 1.0)
+            @inbounds Δm = m
         end
         @inbounds states[i, j] = Int(sign(x[i, j]))
     end
@@ -33,22 +34,25 @@ end
 This is experimental function to run simulations and test ideas.
 """
 function cuda_evolve_optical_oscillators(
-    kpo::KerrOscillators{T},
-    dyn::KPODynamics{T},
+    opo::OpticalOscillators{T},
+    dyn::OPODynamics{T},
     num_rep = 512,
     threads_per_block = (16, 16)
 ) where T <: Real
-    N = nv(kpo.ig)
-    L = N + 1
+    L = nv(opo.ig)
 
-    JK = CUDA.CuArray(kerr_adjacency_matrix(kpo.ig))
+    J = CUDA.CuArray(couplings(opo.ig))
+    h = CUDA.CuArray(biases(opo.ig))
+    JO = -(J + transpose(J))
 
     σ = CUDA.zeros(Int, L, num_rep)
     x = CUDA.CuArray(2 .* rand(L, num_rep) .- 1)
     y = CUDA.CuArray(2 .* rand(L, num_rep) .- 1)
 
     iparams = CUDA.CuArray([dyn.num_steps, num_rep])
-    fparams = CUDA.CuArray([dyn.dt, kpo.detuning, kpo.kerr_coeff, kpo.scale])
+    fparams = CUDA.CuArray([α, opo.scale])
+
+    noise = CUDA.CuArray(rand(opo.noise. L, num_rep))
     pump = CUDA.CuArray([kpo.pump(dyn.dt * (i-1)) for i ∈ 1:dyn.num_steps+1])
 
     th = threads_per_block
@@ -56,7 +60,9 @@ function cuda_evolve_optical_oscillators(
 
     @time begin
         CUDA.@sync begin
-            @cuda threads=th blocks=bl opo_kernel(x, y, σ, JK, pump, fparams, iparams)
+            @cuda threads=th blocks=bl opo_kernel(
+                x, y, σ, JO, pump, noise, fparams, iparams
+            )
         end
     end
 
@@ -64,10 +70,7 @@ function cuda_evolve_optical_oscillators(
     th = prod(threads_per_block)
     bl = ceil(Int, num_rep / th)
 
-    J = CUDA.CuArray(couplings(kpo.ig))
-    h = CUDA.CuArray(biases(kpo.ig))
     energies = CUDA.zeros(num_rep)
-
     @time begin
         CUDA.@sync begin
             @cuda threads=th blocks=bl kerr_energy_kernel(J, h, energies, σ)
@@ -78,7 +81,7 @@ function cuda_evolve_optical_oscillators(
 
     # energy CPU
     σ_cpu = Array(σ)
-    states = [σ_cpu[end, i] * σ_cpu[1:end-1, i] for i ∈ 1:size(σ_cpu, 2)]
+    states = [σ_cpu[:, i] for i ∈ 1:size(σ_cpu, 2)]
     @time en = minimum(energy(states, kpo.ig))
 
     en, en0
